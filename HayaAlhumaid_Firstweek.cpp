d744b5d1cda017ad000b240dcca4a6386dfd15d3
Haya Alhumaid <hayaalhumaid@Hayas-MacBook-Pro.local>
2018-08-22 19:32:51 -0500

    readMe

---
+++ HayaAlhumaid_Firstweek.cpp
@@ -0,0 +1,17 @@
+//
+//  HayaAlhumaid_Firstweek.cpp
+//  
+//
+//  Created by Haya Alhumaid on 8/22/18.
+//
+//
+
+#include <stdio.h>
+using namespace std
+
+int main(){
+    
+    cout<< "Hello World!" << endl;
+    
+    return 0;
+}
