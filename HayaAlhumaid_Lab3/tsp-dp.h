ee11994e0f1baa03f325bb3031c83e440a3565d4
Hayaalhumaid <hkalhumaid@smu.edu>
2018-11-07 14:09:26 -0600

    remove calculate

--- HayaAlhumaid_Lab3/tsp-dp.h
+++ HayaAlhumaid_Lab3/tsp-dp.h
@@ -7,7 +7,6 @@ class TSP{
 public:
 	//double tspNaive(const dist_vec& cities, int pos, int visited, dist_vec& state, vector<int>& cycle);
 	double travllingSalesmanProblem(const dist_vec& graph, int s, vector<int> &Path);
-	void TSP::calculate(int cities[], int route[], dist_vec dist, int i=1);
 	int n;
 	
 	void tspDP(dist_vec dist, int n);
